7ba673eb33674747aa13fe5901edb4c05d026a91
catcuts <354151733@qq.com>
2018-03-21 15:14:16 +0800

    change CRLF to LF

--- special/for_factoryguy/special_mount_disk.sh
+++ special/for_factoryguy/special_mount_disk.sh
@@ -2,8 +2,23 @@
 
 echo "开始 ..."
 
-    bash stop.sh
-    bash mount_disk.sh
-    bash stop.sh -h
+bash ./stop.sh
+bash ./mount_disk.sh
 
-echo "结束. 正在关机 ..."
+echo "硬盘已挂载，请检查:"
+df -h
+
+echo -n "重启/关机/再来一次？:[r/h/p]"       #-n选项移调末尾换行符，不换行
+read cmd
+if [ $cmd == "r" ]; then 
+    bash ./stop.sh -r
+    echo "结束. 正在重启 ..."
+fi
+if [ $cmd == "h" ]; then 
+    bash ./stop.sh -h 
+    echo "结束. 正在关机 ..."
+fi
+if [ $cmd == "p" ]; then 
+    bash ./special_mount_disk.sh
+    echo "结束. 正在重来 ..."
+fi
